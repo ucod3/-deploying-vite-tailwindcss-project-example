90d586f2a44384709bc42d7b7981a8c5bbf1e0ea
Usman <usman.butt@outlook.com>
2023-03-12 23:37:18 -0400

    first commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/deploying-vite-tailwindcss-project-example/',
+  server: {
+    open: true
+  }
+});
