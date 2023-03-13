d5785de9a3b95642c3a4e41d5f0ca527a7674e39
Usman <usman.butt@outlook.com>
2023-03-12 23:43:40 -0400

    changes to vite config and added github workflow

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-// https://vitejs.dev/config/
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/deploying-vite-tailwindcss-project-example/',
   server: {
     open: true
   }
